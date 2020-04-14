93e6f4c41108edd6cc9b1393163151680c5e11b6
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-04-14 14:23:08 -0400

    environment setup 1

--- db/migrate/20200414181208_create_characters.rb
+++ db/migrate/20200414181208_create_characters.rb
@@ -1,4 +1,16 @@
 class CreateCharacters < ActiveRecord::Migration[6.0]
-  def change
+  def change  
+    create_table :characters do |t|
+        t.integer :user_id
+        t.string :name
+        t.string :type
+        t.integer :age
+        t.integer :hp
+        t.string :panic_cry
+        t.integer :item_1_id
+        # t.integer :item_2_id
+        # t.integer :item_3_id
+        t.string :endgame, default: "lose"
+    end
   end
 end
