7d72ef520ab4cc04ca3aeb0b587dac374d520ac9
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-04-17 08:24:57 -0400

    cleaner

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,9 @@ class User < ActiveRecord::Base
         end 
     end 
 
+
+    
+
     def your_a_teen
         system("clear")
         puts "TOILET PAPER IS LAME. WHY DOES #{self.important_person} EVEN NEED IT?"
@@ -161,218 +164,6 @@ class User < ActiveRecord::Base
 
 
 
-
-
-
-    # def item_problem_before_store 
-    #     sleep(2)
-    #     puts "#{self.name} APPROACHES THE STORE"
-    #     puts "  "
-    #     puts "THERE IS A LINE AND A MAN IN A HAZMAT SUIT LETTING PEOPLE IN ONE AT A TIME"
-    #     puts "  "
-    #     if self.purell != nil 
-    #         puts "#{self.name} GETS IN LINE TRYING TO STAY 6 FEET AWAY FROM THE PERSON IN FRONT OF YOU"
-    #         puts " "
-    #         choices = ["APPLY MY PURELL!", "OFFER YOUR PURELL TO THE STRANGERS IN FRONT OF YOU"]
-    #         response = @@prompt.multi_select('WHAT DO YOU DO?', choices, min: 1, max: 1)
-    #         sleep(2)
-    #         case response[0]
-    #         when "APPLY MY PURELL!"
-    #             apply_purell
-    #             sleep(2)
-    #         when "OFFER YOUR PURELL TO THE STRANGERS IN FRONT OF YOU"
-    #             puts " "
-    #             puts "A STRANGER THANKS YOU BY TOUCHING YOUR ARM AND GIVES YOU THE VIRUS"
-    #             puts "  "
-    #             puts "FIVE HEATLH POINTS DEDUCTED"
-    #             self.hp -= 4 
-    #             sleep(2)
-    #         end 
-    #     elsif self.rubber_glove != nil 
-    #         puts "  "
-    #         puts "THE PERSON IN FRONT OF YOU IS COUGHING"
-    #         puts " "
-    #         choices = ["RUN THE OTHER WAY", "TURN YOUR SCARF INTO A MASK"]
-    #         response = @@prompt.multi_select('WHAT DO YOU DO?', choices, min: 1, max: 1)
-    #         sleep(2)
-    #         case response[0]
-    #         when "RUN THE OTHER WAY"
-    #             puts " "
-    #             puts "GOOD THINKING! YOU HAVE AVOIDED THE VIRUS BUT FAILED TO BRING YOUR #{self.important_person} TOILET PAPER."
-    #             sleep(2)
-    #             self.hp -= 70 
-
-    #         when "TURN YOUR SCARF INTO A MASK"
-    #             puts " "
-    #             puts "GREAT CHOICE! NO HEALTH POINTS DEDUCTED"
-    #             sleep(2)
-    #         end 
-
-    #     elsif self.face_mask != nil
-    #         sleep(2)
-    #         puts "GOOD THING YOUR FACE IS PROTECHED"
-    #         puts " "
-    #         choices = ["POCKETS!!!", "WHO CARES? I HAVE A FACEMASK"]
-    #         response = @@prompt.multi_select('WHAT DO YOU DO? WITH YOUR HANDS', choices, min: 1, max: 1)
-    #         puts " "
-    #         case response[0]
-    #         when "POCKETS!!!"
-    #             puts " "
-    #             puts "GOOD THINKING. NO HEALTH POINTS DEDCUTED"
-    #             puts " "
-    #             sleep(2)
-    #         when "WHO CARES? I HAVE A FACEMASK"
-    #             puts " "
-    #             puts "THE VIRUS GETS ON YOUR HANDS AND YOU SPREAD IT TO #{self.important_person}"
-    #             puts "  "
-    #             puts "LOSE 4 HEALTH POINTS "
-    #             sleep(2)
-    #             self.hp -= 4 
-    #         end 
-            
-    #     end 
-    #     self.buying_toilet_paper 
-    #     self.death_status
-    # end 
-
-
-
-
-
-
-    # def item_problem 
-    #     sleep(2)
-    #     if self.purell != nil 
-    #         puts "#{self.name.upcase} PUTS ON THIER SHOES AND WIPES DOWN THE DOOR HANDLE ON THEIR WAY OUT"
-    #         puts " "
-    #         choices = ["APPLY MY PURELL!", "NO NEED FOR PURELL, I JUST WIPED DOWN THE DOOR HANDLE"]
-    #         response = @@prompt.multi_select('WHAT IS YOUR NEXT MOVE?', choices, min: 1, max: 1)
-    #         case response[0]
-    #         when "APPLY MY PURELL!"
-    #             apply_purell
-    #         when "NO NEED FOR PURELL, I JUST WIPED DOWN THE DOOR HANDLE"
-    #             puts " "
-    #             puts "YOURE CARING THE VIRUS AND WILL NOW INFECT 3 PEOPLE"
-    #             puts "  "
-    #             puts "FIVE HEATLH POINTS DEDUCTED"
-    #             sleep(2)
-    #             self.hp -= 4 
-    #         end 
-    #     elsif self.rubber_glove != nil 
-    #         puts "  "
-    #         puts "DONT FORGET TO TAKE OFF YOUR GLOVES BEFORE SEEING #{self.important_person}"
-    #         puts " "
-    #         puts "NO HEATLH POINTS DEDUCTED"
-    #         sleep(2)
-
-    #     elsif self.face_mask != nil
-    #         puts "YOUR FACEMASK IS NOT COMFORTABLE"
-    #         puts " "
-    #         choices = ["NOTHING, IM NOT WEARING GLOVES", "ADJUST IT! IM GOING TO NEED TO WEAR THIS THE WHOLE TRIP!"]
-    #         response = @@prompt.multi_select('WHAT DO YOU DO?', choices, min: 1, max: 1)
-    #         puts " "
-    #         case response[0]
-    #         when "NOTHING, IM NOT WEARING GLOVES"
-    #             puts " "
-    #             puts "GOOD THINKING. NO HEALTH POINTS DEDCUTED"
-    #             puts " "
-    #             sleep(2)
-    #         when "ADJUST IT! IM GOING TO NEED TO WEAR THIS THE WHOLE TRIP!"
-    #             puts " "
-    #             puts "IF THE VIRUS WAS ON YOUR HANDS YOUR FACEMASK HAS BEEN COMPRIMISED AND RENDERED USELESS"
-    #             puts "  "
-    #             puts "LOSE 4 HEALTH POINTS "
-    #             sleep(2)
-    #             self.hp -= 4 
-    #         end 
-            
-    #     end 
-    #     self.death_status
-    # end 
-
-#     def self.graveyard 
-#        person = self.all.last.name 
-#     puts "        _  /)"
-#     puts "        mo / )"
-#     puts "        |/)\)"
-#     puts "         /\_"
-#     puts "         \__|="
-#     puts "        (    )"
-#     puts "        __)(__"
-#     puts "  _____/      \\_____"
-#     puts " |  _     ___   _   ||"
-#     puts " | | \     |   | \  ||"
-#     puts " | |  |    |   |  | ||"
-#     puts " | |_/     |   |_/  ||"
-#     puts " | | \     |   |    ||"
-#     puts " | |  \    |   |    ||"
-#     puts " | |   \. _|_. | .  ||"
-#     puts " |                  ||"
-#     puts " |  #{person}  ||"
-#     puts " |                  ||"
-# puts "*       | *   **    * **   |**      **"
-# puts "\))ejm97/.,(//,,..,,\||(,,.,\\,.((//"  
-#     end 
-
-
-    # def buying_toilet_paper 
-    # system("clear")
-    # @@prompt.warn(".             .        .     .     |--|--|--|--|--|--|")
-    # @@prompt.warn(".                   _______|__|__|__|__|__|_ |  |===||")
-    # @@prompt.warn("__ *            .   )______________________________|-|")
-    # @@prompt.warn("__|  .      .   .  ))______________________________| |")
-    # @@prompt.warn("__|   )||      _|_|))       ooooooooooooooooooooo  |-|")
-    # @@prompt.warn("__|  |)|||__   ||||),-------8                   8 -| |")
-    # @@prompt.warn("__|._|)|||||.| |[=|),-------8  [ B O D E G A ]  8 -|-|")
-    # @@prompt.warn("__|[+|-|-|||||||[=|---------8                   8 -| |")
-    # @@prompt.warn("_-----.|)| )):|_[=||`-------8                   8 -|-|")
-    # @@prompt.warn(")|  )||))8) :  8_||`------  8ooooooooooooooooooo8 -| |")
-    # @@prompt.warn(")=| ))||) |  .  | |||_____________  ____  _________|-|")
-    # @@prompt.warn("==|))||  )   .   | ||_____________ |X|  | _________| |")
-    # @@prompt.warn("==| ||  )         | |_____________ |X| || _________| |")
-    # @@prompt.warn("==| |~ )     .     |")
-    # @@prompt.warn("==|)  )             |________________________________")
-    # puts " "
-    # puts " "
-    # puts "FINALLY #{self.name.upcase} YOU HAVE MADE IT TO THE DELI"
-    # puts " "
-    # sleep(2)
-    # puts "TO YOUR DISMAY THE STORE IS ALMOST OUT OF TOILET PAPER"
-    # puts " "
-    # sleep(2)
-    # toilet_choices
-    # end 
-
-#     def toilet_choices 
-#      choices = ["SINGLE PLY", "FANCY CHARMIN", "BODEGA BRAND RECYLEABLE"]
-#      paper = @@prompt.multi_select("WHICH DO YOU PICK:", choices, min: 1, max: 1)
-#     case paper[0]
-#     when "SINGLE PLY"
-#         puts " "
-#         puts "YOUR #{self.important_person} IS GOING TO LOVE IT!"
-#         puts " "
-#         sleep(2)
-#         puts "CONGRADUATIONS YOUR GOT YOUR TOILET PAPER AND WON THE GAME"
-#         #holding up toilet paper art?
-#     when "FANCY CHARMIN"
-#         puts " "
-#         puts "YOU PICK UP THE TOILET PAPER BUT THE BODEGA CAT HAS ALREADY GOTTEN TO IT"
-#         puts " "
-#         sleep(2)
-#         puts "YOU SNEEZE AND TOUCH YOUR FACE"
-#         puts " "
-#         sleep(2)
-#         puts "YOU HAVE BEEN INFECTED"
-#         self.hp -= 40
-#     when "BODEGA BRAND RECYLEABLE"
-#         puts " "
-#       puts "ODD CHOICE! CONGRADULATIONS YOU HAVE SUCCESSFUL GOTTEN #{self.important_person} THEIR TOILET PAPER AND WON THE GAME"
-#       #holding up toilet paper art?
-#      end 
-#      sleep(3)
-#     self.death_status
-#    end 
-
     def self.welcome
         system("clear")
         @@prompt.error("  /000000   /000000  /00    /00 /000000 /0000000         /000000  /00   /00 /00000000  /000000  /00000000")
@@ -403,43 +194,16 @@ class User < ActiveRecord::Base
         panicCry = @@prompt.multi_select("SELECT YOUR PANIC CRY:", choices_2, min: 1, max: 1)
    
         if char[0] == "TEEN"
-            #do we create a new instance of character? 
-            person = User.create( name: username.upcase, hp: 10, panic_cry: panicCry[0], character_type: "TEEN")
-            #person.start_story
-            #person.see_a_friend
-            #person.buying_toilet_paper
-
-
+            person = User.create( name: username.upcase, hp: 10, panic_cry: panicCry[0], character_type: "TEEN", important_person: "YOUR GRANDMOTHER")
         elsif char[0] == "SENIOR"
-            #create a new instance of character?
-            person = User.create( name: username.upcase, hp: 5, panic_cry: panicCry[0], character_type: "SENIOR")
-           # person.start_story
-            #person.see_a_cute_dog
-            #person.buying_toilet_paper
-            
-
+            person = User.create( name: username.upcase, hp: 5, panic_cry: panicCry[0], character_type: "SENIOR", important_person: "YOUR ELDERLY MOTHER")
         elsif char[0] == "ADULT"
-            #create a new instance of character?
-            person = User.create(name: username.upcase, hp: 7, panic_cry: panicCry[0], character_type: "ADULT")
-           # person.start_story
-            #person.encounter_hobo
-            #person.buying_toilet_paper
-            #the game seems short if we limit one encounter per character 
-
+            person = User.create(name: username.upcase, hp: 7, panic_cry: panicCry[0], character_type: "ADULT", important_person: "YOUR DISABLED ADULT CHILD")
         end
     end
 
 
     def start_story
-
-        if self.hp == 10
-            self.important_person = "YOUR GRANDMOTHER"
-        elsif self.hp == 7
-            self.important_person = "YOUR ELDERLY MOTHER"
-        elsif self.hp == 5
-            self.important_person = "YOUR DISABLED ADULT CHILD"
-        end 
-    
         system("clear")
         @@prompt.warn(":::888888888888888888888888888888888888***8888888888888888888888::::88")
         @@prompt.warn("::::8888888888888888888888P   ____.------.____   488888888888888:::888")
@@ -507,21 +271,8 @@ class User < ActiveRecord::Base
             item = FaceMask.create
             self.face_mask = item
         end 
-       
-        # case response[1]
-        
-        # when "PURELL" 
-        #     item = Purell.create 
-        #     self.purell = item
-        # when "RUBBER GLOVES"
-        #     item = RubberGlove.create
-        #     self.rubber_glove = item
-        # when "FACE MASK"
-        #     item = FaceMask.create
-        #     self.face_mask = item
-        # end 
       puts " "
-      puts "#{self.name} GRABS THE ITEMS AND LEAVES!"
+      puts "YOU GRAB IT AND LEAVE!"
       puts " "
       sleep(2)
     end 
@@ -581,6 +332,7 @@ class User < ActiveRecord::Base
             sleep(2)
             self.hp -= 4 
         end 
+        puts "YOUR HEALTH IS NOW AT: #{self.hp}" 
     end 
 
 
@@ -778,28 +530,26 @@ class User < ActiveRecord::Base
         puts "                                                "
         sleep(2)
         if response[0] == "USE PANIC CRY!"
-            if self.panic_cry == "HELP!"
-                puts "#{self.name}: HELP!"
-                puts " "
-                sleep(2)
-                puts "THE HOBO TRIES TO COVER YOUR MOUTH BUT YOU MANAGE TO ESCAPE AND RUN AWAY. LOSE 4 HEALTH POINTS"
+            "#{self.name}: #{self.panic_cry}"
+            puts "  "
+            sleep(2)
+            case self.panic_cry
+            when "HELP!"
+                puts "HE TRIES TO COVER YOUR MOUTH BUT YOU MANAGE TO ESCAPE AND RUN AWAY. LOSE 4 HEALTH POINTS"
                 self.hp -= 4
-            elsif self.panic_cry == "GET A JOB!"
-                puts "#{self.name}: GET A JOB!"
-                puts " "
                 sleep(2)
-                puts "THE HOBO STOPS AND EXPLAINS THAT HE IS ACTUALLY A RHODES SCHOLAR AND HIS LIFE CHOICES ARE NON OF YOUR CONCERN"
+            when "GET A JOB!"
+                puts "HE STOPS AND EXPLAINS THAT HE IS ACTUALLY A RHODES SCHOLAR AND HIS LIFE CHOICES ARE NON OF YOUR CONCERN"
                 puts " "
                 puts "NO HEALTH POINTS DEDUCTED AND YOU LEARN A VALUABLE LESSON"
-            elsif self.panic_cry == "(COUGH, COUGH)"
-                puts "#{self.name}: (COUGH,COUGH)"
-                puts " "
                 sleep(2)
-                puts "THE HOBO BECOMES AFRAID THAT YOU MIGHT HAVE THE VIRUS AND KNOCKS YOU OVER. LOSE 1 HEALTH POINT"
+            when "(COUGH, COUGH)"
+                puts "HE BECOMES AFRAID THAT YOU MIGHT HAVE THE VIRUS AND KNOCKS YOU OVER. LOSE 1 HEALTH POINT"
+                sleep(2)
                 self.hp -= 1
             end
         else
-            puts "THE HOBO GASPS AND YOU MAKE A CLEAN GETAWAY. NO HEALTH POINTS DEDUCTED"
+            puts "HE GASPS AND YOU MAKE A CLEAN GETAWAY. NO HEALTH POINTS DEDUCTED"
         end
         sleep(2)
         puts "                                                "
@@ -871,7 +621,7 @@ class User < ActiveRecord::Base
 
             when "TURN YOUR SCARF INTO A MASK"
                 puts " "
-                puts "SMART MOVE! YOU'VE PROTECTED YOURSELF FROM CATCHING THE NO HEALTH POINTS DEDUCTED"
+                puts "SMART MOVE! YOU'VE PROTECTED YOURSELF FROM CATCHING THE VIRUS NO HEALTH POINTS DEDUCTED"
                 sleep(2)
             end 
 
